bb84e4c26f8cc99af2f3ecb0513822bfd6cf053e
Vasile Papuc <vasile.papuc@binarcode.com>
2021-07-20 09:53:09 +0300

    Define route for restify

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\Restify\Auth\ForgotPasswordController;
+use App\Http\Controllers\Restify\Auth\LoginController;
+use App\Http\Controllers\Restify\Auth\RegisterController;
+use App\Http\Controllers\Restify\Auth\ResetPasswordController;
+use App\Http\Controllers\Restify\Auth\VerifyController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,3 +13,22 @@ Route::restifyAuth();
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('register', RegisterController::class)
+    ->name('restify.register');
+
+Route::post('login', LoginController::class)
+    ->middleware('throttle:6,1')
+    ->name('restify.login');
+
+Route::post('verify/{id}/{hash}', VerifyController::class)
+    ->middleware('throttle:6,1')
+    ->name('restify.verify');
+
+Route::post('forgotPassword', ForgotPasswordController::class)
+    ->middleware('throttle:6,1')
+    ->name('restify.forgotPassword');
+
+Route::post('resetPassword', ResetPasswordController::class)
+    ->middleware('throttle:6,1')
+    ->name('restify.resetPassword');
